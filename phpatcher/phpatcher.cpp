a3085c6ebeb89fab9b2b7cf6f3b27ec7d0876397
hessfire <134144364+hessfire@users.noreply.github.com>
2023-11-27 17:41:15 +0200

    major update

--- phpatcher/phpatcher.cpp
+++ phpatcher/phpatcher.cpp
@@ -4,6 +4,7 @@
 #include <ctime>
 #include <string.h>
 #include <sstream>
+#include <iomanip>
 
 using namespace std;
 
@@ -13,7 +14,13 @@ enum processHackerOffsets64
 	ClassOffset64 = 0x129100,
 	DescriptionOffset64 = 0x19EAE4,
 	IconOffset64 = 0x17CB18,
-	qword_140159D10 = 0x20A1D
+	qword_140159D10 = 0x20A1D,
+	AppendAdminOffset64 = 0x227A1,
+	PhTreeNewOffset64 = 0x129A70,
+	MenuHackerOffset64 = 0x18F6E0,
+	MenuProcessesOffset64 = 0x129A18,
+	MenuServicesOffset64 = 0x129A30,
+	MenuNetworkOffset64 = 0x129A48
 };
 
 template<typename t>
@@ -32,7 +39,20 @@ t gen_random(int len) {
 	return tmp_s;
 }
 
-void patch_bytes(fstream* filestream, int offset, std::string bytes_str)
+string read_bytes(fstream* filestream, int offset, int length)
+{
+	filestream->seekg(offset, ios::beg);
+	ostringstream hex_ss;
+	hex_ss << hex << setfill('0');
+
+	for (int i = 0; i < length; ++i) {
+		hex_ss << setw(2) << unsigned((unsigned char)(filestream->get()));
+	}
+
+	return hex_ss.str();
+}
+
+void patch_bytes(fstream* filestream, int offset, string bytes_str)
 {
 	filestream->seekg(offset, ios::beg);
 
@@ -47,7 +67,7 @@ void patch_bytes(fstream* filestream, int offset, std::string bytes_str)
 
 	string result(begin(bytes), end(bytes));
 
-	filestream->write(result.c_str(), result.length() * 2);
+	filestream->write(result.c_str(), result.length());
 }
 
 void patch_string(fstream* filestream, int offset, wstring string)
@@ -63,6 +83,15 @@ void add_junk(fstream* filestream)
 	filestream->write(rnd.c_str(), rnd.length());
 }
 
+string gen_nop_string(int length)
+{
+	string out = "";
+	for (int i = 0; i < length; i++)
+		out += "90"; /*0x90 = nop*/
+	out.resize(length * 2);
+	return out;
+}
+
 int main()
 {
 	srand(time(0));
@@ -73,19 +102,80 @@ int main()
 	wstring name_class = gen_random<wstring>(13);
 	wstring description = gen_random<wstring>(14);
 	wstring icon = gen_random<wstring>(14);
+	wstring phtreenew = gen_random<wstring>(9);
+	wstring mhacker = gen_random<wstring>(6);
+	wstring mprocesses = gen_random<wstring>(9);
+	wstring mservices = gen_random<wstring>(8);
+	wstring mnetwork = gen_random<wstring>(7);
 
 	if (!_64.is_open())
 	{
-		MessageBoxA(0, "failed to open 64.exe, make sure to rename your processhacker(x64 version) to 64.exe", 0, MB_ICONERROR);
+		cout << "[-] failed to open 64.exe, make sure to rename your processhacker(x64 version) to 64.exe" << endl;
+		cin.get();
 		return 1;
 	}
+
+	string qword_140159D10_val = read_bytes(&_64, qword_140159D10, 0x2);
+	string AppendAdminOffset64_val = read_bytes(&_64, AppendAdminOffset64, 0x18);
+
+	cout << "[*] 0x20A1D | " << qword_140159D10_val << endl;
+	cout << "[*] 0x227A1 | " << AppendAdminOffset64_val << endl;
+
+	if (qword_140159D10_val.find("4889") == string::npos)
+	{
+		cout << "[*] this file is already patched, no need to patch again. aborting..." << endl;
+		cin.get();
+		return 1;
+	}
+
+	cout << "[*] applying patches..." << endl;
 
 	patch_string(&_64, TitleOffset64, name.c_str());
 	patch_string(&_64, ClassOffset64, name_class.c_str());
 	patch_string(&_64, DescriptionOffset64, description.c_str());
 	patch_string(&_64, IconOffset64, icon.c_str());
+	patch_string(&_64, PhTreeNewOffset64, phtreenew.c_str());
+	patch_string(&_64, MenuHackerOffset64, mhacker.c_str());
+	patch_string(&_64, MenuProcessesOffset64, mprocesses.c_str());
+	patch_string(&_64, MenuServicesOffset64, mservices.c_str());
+	patch_string(&_64, MenuNetworkOffset64, mnetwork.c_str());
 
-	patch_bytes (&_64, qword_140159D10, "9090" /*0x90 = nop*/);
+	patch_bytes (&_64, qword_140159D10, gen_nop_string(0x2));
+	patch_bytes (&_64, AppendAdminOffset64, gen_nop_string(0x18));
+	/*
+		lea rdx, aAdministrator ; " (Administrator)"
+		lea rcx, [rbp+57h+var_70]
+		lea r8, [rax+rax]
+		call PhAppendStringBuilderEx
+
+		nop allat stuff
+	*/
+
+	cout << "[+] patched " << hex << TitleOffset64 << dec << endl;
+	cout << "[+] patched " << hex << ClassOffset64 << dec << endl;
+	cout << "[+] patched " << hex << DescriptionOffset64 << dec << endl;
+	cout << "[+] patched " << hex << IconOffset64 << dec << endl;
+	cout << "[+] patched " << hex << PhTreeNewOffset64 << dec << endl;
+	cout << "[+] patched " << hex << MenuHackerOffset64 << dec << endl;
+	cout << "[+] patched " << hex << MenuProcessesOffset64 << dec << endl;
+	cout << "[+] patched " << hex << MenuServicesOffset64 << dec << endl;
+	cout << "[+] patched " << hex << MenuNetworkOffset64 << dec << endl;
+
+	cout << "[+] patched " << hex << qword_140159D10 << dec << endl;
+	cout << "[+] patched " << hex << AppendAdminOffset64 << dec << endl;
 
 	add_junk(&_64);
+
+	qword_140159D10_val = read_bytes(&_64, qword_140159D10, 0x2);
+	AppendAdminOffset64_val = read_bytes(&_64, AppendAdminOffset64, 0x18);
+
+	_64.flush();
+	_64.close();
+
+	cout << "[+] done!" << endl;
+
+	cout << "[*] 0x20A1D | " << qword_140159D10_val << endl;
+	cout << "[*] 0x227A1 | " << AppendAdminOffset64_val << endl;
+
+	cin.get();
 }
